5cff56b8e15d3dbf2a5479c6e79712a14c1109d5
piotrparzysz <piciolo@outlook.com>
2023-11-29 13:18:04 +0100

    add query

--- src/main/java/edu/put/ClientApplication.java
+++ src/main/java/edu/put/ClientApplication.java
@@ -1,4 +1,28 @@
 package edu.put;
 
+import edu.put.backend.BackendConfig;
+import edu.put.backend.BackendException;
+import edu.put.backend.BackendSession;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+@Slf4j
 public class ClientApplication {
+
+    public static void main(String[] args) throws BackendException {
+        var config = new BackendConfig();
+        var backendSession = new BackendSession(config.getContactPoint(), config.getKeyspace());
+        var session = backendSession.getSession();
+        for (int i = 0; i < 100; i++) {
+            String query = String.format("INSERT INTO client_orders (client_id, creation_time, price, food) VALUES (%s, %s, %s, %s)",
+                    i,
+                    new Date(),
+                    100,
+                    "eggs");
+            log.info("Running query: {}", query);
+            session.execute(query);
+        }
+    }
+
 }
