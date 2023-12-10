5d6140c9e6b8c6381eabdb51aef3ea268418f1ff
KanarXD <piciolo@outlook.com>
2023-12-10 17:21:44 +0100

    add all tables

--- src/main/java/edu/put/backend/BackendInitScripts.java
+++ src/main/java/edu/put/backend/BackendInitScripts.java
@@ -4,25 +4,33 @@ import java.util.List;
 
 public class BackendInitScripts {
     public static final List<String> SCRIPTS = List.of(
-            "DROP TABLE client_orders;",
-            "DROP TABLE ready_orders;",
+            "DROP TABLE IF EXISTS client_orders;",
+            "DROP TABLE IF EXISTS prepared_orders;",
+            "DROP TABLE IF EXISTS ready_orders;",
             """
                     CREATE TABLE client_orders (
-                    client_id INT,
-                    creation_time timestamp,
-                    price INT,
+                    food_category VARCHAR,
+                    creation_time TIMESTAMP,
+                    user_id INT,
                     food VARCHAR,
-                    PRIMARY KEY (client_id, creation_time)
+                    order_id UUID,
+                    PRIMARY KEY (food_category, creation_time)
+                    ) WITH CLUSTERING ORDER BY (creation_time DESC);""",
+            """
+                    CREATE TABLE prepared_orders (
+                    order_id UUID,
+                    creation_time TIMESTAMP,
+                    restaurant_id INT,
+                    price INT,
+                    info VARCHAR,
+                    PRIMARY KEY (order_id, creation_time)
                     ) WITH CLUSTERING ORDER BY (creation_time DESC);""",
             """
                     CREATE TABLE ready_orders (
-                    client_id INT,
-                    creation_time timestamp,
-                    restaurant_id INT,
-                    price INT,
-                    food VARCHAR,
-                    PRIMARY KEY (client_id, creation_time)
-                    ) WITH CLUSTERING ORDER BY (creation_time DESC);"""
+                    id INT,
+                    info VARCHAR,
+                    PRIMARY KEY (id)
+                    );"""
     );
 
 }
