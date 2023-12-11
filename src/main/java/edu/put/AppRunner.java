6c22ce09949903930d7a27edd4e0a4dba5a1a0aa
piotrparzysz <piciolo@outlook.com>
2023-12-11 13:31:51 +0100

    add AppRunner

---
+++ src/main/java/edu/put/AppRunner.java
@@ -0,0 +1,22 @@
+package edu.put;
+
+public class AppRunner {
+
+    public static void main(String[] args) throws InterruptedException {
+        var clientAppCount = 2;
+        var clientApps = new ClientApplication[clientAppCount];
+
+        for (int i = 0; i < clientAppCount; i++) {
+            var clientApplication = new ClientApplication(i);
+            clientApplication.start();
+            clientApps[i] = clientApplication;
+        }
+
+        for (int i = 0; i < clientAppCount; i++) {
+            clientApps[i].join();
+        }
+
+    }
+
+
+}
--- src/main/java/edu/put/ClientApplication.java
+++ src/main/java/edu/put/ClientApplication.java
@@ -3,30 +3,35 @@ package edu.put;
 import com.datastax.driver.core.Session;
 import edu.put.backend.BackendConfig;
 import edu.put.backend.BackendSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Slf4j
-public class ClientApplication {
-    private static int clientId;
-    private static Session session;
+@RequiredArgsConstructor
+public class ClientApplication extends Thread {
+    private final int clientId;
+    private Session session;
 
-    public static void main(String[] args) throws Exception {
-        clientId = 0;
-        var config = new BackendConfig();
-        var backendSession = new BackendSession(config.getContactPoint(), config.getKeyspace());
-        session = backendSession.getSession();
-        for (int i = 0; i < 100; i++) {
-            insertClientOrder();
+    @Override
+    public void run() {
+        try {
+            var config = new BackendConfig();
+            var backendSession = new BackendSession(config.getContactPoint(), config.getKeyspace());
+            session = backendSession.getSession();
+            for (int i = 0; i < 100; i++) {
+                insertClientOrder();
 //            Thread.sleep(100);
+            }
+        } catch (Exception e) {
+            log.error("Thread: {}, error: {}", clientId, e.getMessage());
         }
-
         System.exit(0);
     }
 
-    private static void insertClientOrder() {
+    private void insertClientOrder() {
         var query = String.format(
                 "INSERT INTO client_orders (foodCategory, creationTime, userId, food, orderId) VALUES ('%s', '%s', %s, '%s', '%s')",
                 "kebab",
