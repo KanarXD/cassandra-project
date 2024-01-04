cb9d8f034b7e78ee5d0134f63749d4dc94f64913
KanarXD <piciolo@outlook.com>
2024-01-04 19:15:46 +0100

    add random food

--- src/main/java/edu/put/apps/ClientApplication.java
+++ src/main/java/edu/put/apps/ClientApplication.java
@@ -4,14 +4,19 @@ import edu.put.backend.BackendSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
 public class ClientApplication extends Thread {
     private final int clientId;
     private final BackendSession session;
+    private final Random random = new Random();
+    private final Map<String, List<String>> foodMap = Map.of(
+            "kebab", List.of("doner", "small", "medium", "large", "xxl", "spicy"),
+            "pizza", List.of("margherita", "neapolitana", "jalapeno", "piccante", "salami", "hawaii"),
+            "drink", List.of("tequila sunrise", "sex on the beach", "blue lagoon", "margarita", "cosmopolitan")
+    );
 
     @Override
     public void run() {
@@ -25,12 +30,14 @@ public class ClientApplication extends Thread {
     }
 
     private void insertClientOrder() {
+        var foodCategory = foodMap.keySet().stream().toList().get(random.nextInt(foodMap.size()));
+        var food = foodMap.get(foodCategory);
         var query = String.format(
                 "INSERT INTO client_orders (foodCategory, creationTime, userId, food, orderId) VALUES ('%s', '%s', %s, '%s', '%s')",
-                "kebab",
+                foodCategory,
                 new Date().toInstant(),
                 clientId,
-                "plus size",
+                food,
                 UUID.randomUUID()
         );
         log.info("Running query: {}", query);
